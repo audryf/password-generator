96fc3eb02d225e7cdc88cb909882114b5337de42
Audry Ford <audryford792@gmail.com>
2021-07-01 16:27:55 -0500

    password length

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var passwordLength = window.prompt("Between 8 - 128 characters, how long should your password be?");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength);
+  } else {
+    //window.prompt();
+    window.alert("Please choose a valid number.");
+    generatePassword();
+  }
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
