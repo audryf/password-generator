2348fb17431a9b034a52b20cf63ef3b6c2f0bbbe
Audry Ford <audryford792@gmail.com>
2021-07-07 22:19:19 -0500

    create arrays and pool for criteria

--- script.js
+++ script.js
@@ -1,52 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789"
-var symbols = "!@#$%^&*()"
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var pool = [];
+
 
 var criteria = function() {
   // User chooses which character types to include out of lowercase, uppercase, numbers, & symbols
-  lowerCase = window.confirm("Should your password contain any lowercase letters?");
-  console.log(lowerCase);
-    if (lowerCase) {
+  // Should each one of these be their own function?
+  lower = window.confirm("Should your password contain any lowercase letters?");
+  console.log(lower);
+    if (lower) {
       window.confirm("Adding lowercase letters...");
+      pool = pool.concat(lowerCase);
     } else {
       window.confirm("Got it, no lowercase letters.");
     };
-  upperCase = window.confirm("Should your password contain any uppercase letters?");
-  console.log(upperCase);
-    if (upperCase) {
+
+  upper = window.confirm("Should your password contain any uppercase letters?");
+  console.log(upper);
+    if (upper) {
       window.confirm("Adding uppercase letters...");
+      pool = pool.concat(upperCase);
     } else {
       window.confirm("Okay, no uppercase letters added.");
     };
 
-  numbers = window.confirm("Should your password contain any numbers?");
-  console.log(numbers);
-    if (numbers) {
+  nums = window.confirm("Should your password contain any numbers?");
+  console.log(nums);
+    if (nums) {
       window.confirm("Adding numbers to the mix...");
+      pool = pool.concat(numbers);
     } else {
       window.confirm("Alrighty, no numbers here.");
     };
 
-  symbols = window.confirm("Should your password contain any special characters?");
-  console.log(symbols);
-    if (symbols) {
+  syms = window.confirm("Should your password contain any special characters?");
+  console.log(syms);
+    if (syms) {
       window.confirm("Mixing in some special characters...");
+      pool = pool.concat(symbols);
     } else {
       window.confirm("Nothing special thrown in.");
     };
     
-  if (lowerCase === false && upperCase === false && numbers === false && symbols === false) {
+  if (lower === false && upper === false && nums === false && syms === false) {
     // If they do not choose anything, prompt user to select at least one choice
     window.alert("You must choose at least one criteria for a secure password! Please try again.");
     criteria();
   } else {
       window.confirm("Creating password...");
   }
+  console.log(pool);
+  
 }
+
 
 function generatePassword() {
   var passwordLength = window.prompt("Between 8 - 128 characters, how long should your password be?");
